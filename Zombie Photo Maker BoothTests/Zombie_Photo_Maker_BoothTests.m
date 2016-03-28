90e2816a8872da5a51d636cb143c8d052270a781
Muhammad Luqman <MuhammadLuqman@muhammads-macbook-pro.local>
2016-03-28 09:55:43 +0500

    Initial Commit

---
+++ Zombie Photo Maker BoothTests/Zombie_Photo_Maker_BoothTests.m
@@ -0,0 +1,39 @@
+//
+//  Zombie_Photo_Maker_BoothTests.m
+//  Zombie Photo Maker BoothTests
+//
+//  Created by Muhammad Luqman on 3/28/16.
+//  Copyright © 2016 Muhammad Luqman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Zombie_Photo_Maker_BoothTests : XCTestCase
+
+@end
+
+@implementation Zombie_Photo_Maker_BoothTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
